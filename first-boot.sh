c378640b15f746b03928818f56535e584e8b1a42
Cirelli <cirelli94@gmail.com>
2016-03-17 11:10:39 +0100

    Rimossi programmi inutili: firefox e imagemagick

--- first-boot.sh
+++ first-boot.sh
@@ -44,6 +44,11 @@ curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
 
 ################################################################################
+# remove useless programs
+sudo apt-get remove -y \
+	firefox imagemagick
+	
+################################################################################
 # basic update
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
