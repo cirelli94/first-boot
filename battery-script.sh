70f205d88120deb3b9eba6453244f62a92c1207d
Cirelli <cirelli94@gmail.com>
2016-10-18 20:19:42 +0200

    aggiunto file batteryscript.sh

---
+++ battery-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "START BatteryScript"
+echo ""
+
+sudo powertop --auto-tune && echo "set powertop --auto-tune"
+
+xbacklight -set 50 && echo "set xbackglight"
+
+# This set cpufreq and governor every 5 minutes
+while true; do
+	echo ""
+	echo "cpufreq-set " && date "+%H:%M"
+	echo ""
+	sudo cpufreq-set -r --max 1500MHz
+	sudo cpufreq-set -r -g powersave
+	sleep 300
+done
+echo ""
+echo "STOP BatterScript"
