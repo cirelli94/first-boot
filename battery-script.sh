2b0980c4a0e35b94c0eaf8009a54aa8f4171b9f6
Cirelli <cirelli94@gmail.com>
2016-10-27 19:05:13 +0200

    aggiunto if/else per presenza batteria

--- battery-script.sh
+++ battery-script.sh
@@ -9,11 +9,20 @@ xbacklight -set 40 && echo "set xbacklight"
 
 # This set cpufreq and governor every 5 minutes
 while true; do
-	echo "cpufreq-set " && date "+%H:%M"
-	for numcpu in {0..7}
- 	do
-		sudo cpufreq-set -c $numcpu -u 800MHz -g powersave
-	done
+	capacity=`cat /sys/class/power_supply/BAT0/capacity`
+	if [ $capacity -le 100 ]
+	then
+		# If there is battery...
+		echo "BATTERY"
+		echo "cpufreq-set " && date "+%H:%M"
+		for numcpu in {0..7}
+		do
+			sudo cpufreq-set -c $numcpu -u 800MHz -g powersave
+		done
+	else
+		# if there's no battery
+		echo "AC"
+	fi
 	sleep 300
 done
 echo "STOP BatterScript"
