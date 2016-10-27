1bf051ca3c5c4af8cdb7991b4957d7d43b87c756
Cirelli <cirelli94@gmail.com>
2016-10-27 16:41:33 +0200

    uso bash ranges {0..7}

--- battery-script.sh
+++ battery-script.sh
@@ -10,7 +10,7 @@ xbacklight -set 40 && echo "set xbacklight"
 # This set cpufreq and governor every 5 minutes
 while true; do
 	echo "cpufreq-set " && date "+%H:%M"
-	for numcpu in 0 1 2 3 4 5 6 7
+	for numcpu in {0..7}
  	do
 		sudo cpufreq-set -c $numcpu -u 800MHz -g powersave
 	done
