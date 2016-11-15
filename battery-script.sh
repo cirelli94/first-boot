58b5d99e99d5543756e8bb841b6836adb7addbda
Cirelli <cirelli94@gmail.com>
2016-11-15 16:53:05 +0100

    Uso di AC0 invece della capacità della batteria

--- battery-script.sh
+++ battery-script.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
-capacity=`cat /sys/class/power_supply/BAT0/capacity`
-if [ $capacity -le 100 ]
+# TODO add xbacklight
+# 	xbacklight -set 30 && echo "set xbacklight"
+
+DISPLAY=:0.0
+HOME=/home/cirelli/
+XAUTHORITY=$HOME/.Xauthority
+export DISPLAY XAUTHORITY HOME
+
+#check if you run the script as root
+if [ "$(whoami)" != "root" ]; then
+    notify-send -i error "You must to run $(basename $0) script as root."
+    echo "You must to run $(basename $0) script as root."
+    exit
+fi
+
+CHARGING=`cat /sys/class/power_supply/AC0/online`
+if [ $CHARGING == 0 ]
 then
-	# If there is battery...
-	notify-send -t 100000 "START BatteryScript"
+	# DISCHARGING
+	notify-send "Battery Mode"
 	sudo powertop --auto-tune && echo "set powertop --auto-tune"
-	xbacklight -set 40 && echo "set xbacklight"
 else
-	# if there's no battery
-	echo "AC"
-        notify-send -t 100000 "NO BatteryScript"
-	notify-send "No battery, full power mode!"
-	skype &
+	# CHARGING
+	notify-send "NO Battery Mode"
+	skypeforlinux &
 	deluge-gtk &
 fi
 
 # This set cpufreq and governor every 5 minutes
 while true; do
-	capacity=`cat /sys/class/power_supply/BAT0/capacity`
-	if [ $capacity -le 100 ]
+	CHARGING=`cat /sys/class/power_supply/AC0/online`
+	if [ $CHARGING == 0 ]
 	then
-		# If there is battery...
-		echo "BATTERY"
-		echo "cpufreq-set " && date "+%H:%M"
+		# DISCHARGING
+		echo "cpufreq-set "
 		for numcpu in {0..7}
 		do
 			sudo cpufreq-set -c $numcpu -u 800MHz -g powersave
 		done
 	else
-		# if there's no battery
+		# CHARGING
 		echo "AC"
 	fi
 	sleep 300
 done
-echo "STOP BatterScript"
