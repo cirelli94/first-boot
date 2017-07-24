762246a3d0d5d3eb33835a16ad601243a29cfe75
Cirelli <cirelli94@gmail.com>
2017-07-24 18:46:04 +0200

    aggiunto varie.sh: install kodi e download quasar

---
+++ varie.sh
@@ -0,0 +1,6 @@
+wget https://github.com/scakemyer/plugin.video.quasar/releases/download/v0.9.74/plugin.video.quasar-0.9.74.linux_x64.zip
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get install kodi
